75a87428873737397ace36c8fa486a5b18d6df50
dtavares99 <davidtavaresfs@gmail.com>
2025-10-21 20:17:12 -0300

    Fix: Creating db.example

---
+++ config/db.example.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'oci:dbname=//127.0.0.1:1521/FREEPDB1;charset=AL32UTF8',
+    'username' => 'USUARIO_EXEMPLO',
+    'password' => 'SENHA_EXEMPLO',
+    'charset' => 'AL32UTF8',
+];
