9596f4b31bf336bcf7e1cd2acf3fb10be6623827
dtavares99 <davidtavaresfs@gmail.com>
2025-10-22 19:10:43 -0300

    Fix: Order create logic

--- views/order/create.php
+++ views/order/create.php
@@ -5,16 +5,15 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\Order $model */
 
-$this->title = 'Create Order';
-$this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['index']];
+$this->title = 'Adicionar Pedido';
+$this->params['breadcrumbs'][] = ['label' => 'Pedidos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="order-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <?= $this->render('_form', [
         'model' => $model,
+        'clients' => $clients,
     ]) ?>
 
 </div>
