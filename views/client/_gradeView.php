b3f2c0db401e39fa1b0a5917f12c7626e82b6c03
dtavares99 <davidtavaresfs@gmail.com>
2025-10-24 11:59:07 -0300

    Fix: Creating gradeView component to client

---
+++ views/client/_gradeView.php
@@ -0,0 +1,70 @@
+<?php
+
+use app\models\Client;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\GridView;
+
+
+/** @var yii\web\View $this */
+/** @var app\models\OrderSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$isClientView = isset($isClientView);
+
+$columns = [
+        'ID',
+        [
+            'attribute' => 'NAME',
+            'format' => 'raw',
+            'value' => fn($m) => Html::a(Html::encode($m->NAME), ['view', 'ID' => $m->ID]),
+        ],
+        'EMAIL:email',
+        'STATUS',
+        [
+            'attribute' => 'CREATED_AT',
+            'label' => 'Data de cadastro',
+            'format' => 'raw',
+            'value' => function ($m) {
+                $dt = DateTime::createFromFormat('d-M-y h.i.s.u A', $m->CREATED_AT);
+                return $dt ? $dt->format('d/m/Y H:i') : $m->CREATED_AT;
+            },
+        ],
+        [
+            'class' => yii\grid\ActionColumn::class,
+            'header' => 'Ações',
+            'contentOptions' => ['class' => 'text-end'],
+            'template' => '{view} {update} {delete}',
+            'buttons' => [
+                'view' => fn($url) =>
+                    Html::a('<i class="fas fa-eye"></i>', $url, [
+                        'title' => 'Visualizar',
+                        'class' => 'btn btn-sm btn-outline-primary me-1 mb-1',
+                    ]),
+                'update' => fn($url) =>
+                    Html::a('<i class="fas fa-edit"></i>', $url, [
+                        'title' => 'Editar',
+                        'class' => 'btn btn-sm btn-outline-warning me-1 mb-1',
+                    ]),
+                'delete' => fn($url) =>
+                    Html::a('<i class="fas fa-trash"></i>', $url, [
+                        'title' => 'Excluir',
+                        'class' => 'btn btn-sm btn-outline-danger me-1 mb-1',
+                        'data-method' => 'post',
+                        'data-confirm' => 'Tem certeza que deseja excluir?',
+                    ]),
+            ],
+            'urlCreator' => function ($action, Client $model, $key, $index, $column) {
+                return Url::toRoute([$action, 'ID' => $model->ID]);
+            }
+        ],
+    ];
+?>
+
+<?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'headerRowOptions' => ['style' => 'background-color: #f8f9fa;'],
+    'summary' => 'Exibindo <b>{count}</b> de <b>{totalCount}</b> registro(s)',
+    'emptyText' => 'Nenhum registro encontrado.',
+    'columns' => $columns
+]); ?>
--- views/client/index.php
+++ views/client/index.php
@@ -1,11 +1,6 @@
 <?php
 
-use app\models\Client;
 use yii\helpers\Html;
-use yii\helpers\Url;
-use yii\grid\ActionColumn;
-use yii\grid\GridView;
-
 /** @var yii\web\View $this */
 /** @var app\models\ClientSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
@@ -22,59 +17,9 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
     <div class="bg-white p-2 border rounded">
         <div class="table-responsive">
-            <?= GridView::widget([
-                'dataProvider' => $dataProvider,
-                'headerRowOptions' => ['style' => 'background-color: #f8f9fa;'],
-                'summary' => 'Exibindo <b>{count}</b> de <b>{totalCount}</b> registro(s)',
-                'emptyText' => 'Nenhum registro encontrado.',
-                'columns' => [
-                    'ID',
-                    [
-                        'attribute' => 'NAME',
-                        'format' => 'raw',
-                        'value' => fn($m) => Html::a(Html::encode($m->NAME), ['view', 'ID' => $m->ID]),
-                    ],
-                    'EMAIL:email',
-                    'STATUS',
-                    [
-                        'attribute' => 'CREATED_AT',
-                        'label' => 'Data de cadastro',
-                        'format' => 'raw',
-                        'value' => function ($m) {
-                                        $dt = DateTime::createFromFormat('d-M-y h.i.s.u A', $m->CREATED_AT);
-                                        return $dt ? $dt->format('d/m/Y H:i') : $m->CREATED_AT;
-                                    },
-                    ],
-                    [
-                        'class' => yii\grid\ActionColumn::class,
-                        'header' => 'Ações',
-                        'contentOptions' => ['class' => 'text-end'],
-                        'template' => '{view} {update} {delete}',
-                        'buttons' => [
-                            'view' => fn($url) =>
-                                Html::a('<i class="fas fa-eye"></i>', $url, [
-                                    'title' => 'Visualizar',
-                                    'class' => 'btn btn-sm btn-outline-primary me-1 mb-1',
-                                ]),
-                            'update' => fn($url) =>
-                                Html::a('<i class="fas fa-edit"></i>', $url, [
-                                    'title' => 'Editar',
-                                    'class' => 'btn btn-sm btn-outline-warning me-1 mb-1',
-                                ]),
-                            'delete' => fn($url) =>
-                                Html::a('<i class="fas fa-trash"></i>', $url, [
-                                    'title' => 'Excluir',
-                                    'class' => 'btn btn-sm btn-outline-danger me-1 mb-1',
-                                    'data-method' => 'post',
-                                    'data-confirm' => 'Tem certeza que deseja excluir?',
-                                ]),
-                        ],
-                        'urlCreator' => function ($action, Client $model, $key, $index, $column) {
-                                        return Url::toRoute([$action, 'ID' => $model->ID]);
-                                    }
-                    ],
-                ],
-            ]); ?>
+             <?= $this->render('_gradeView', [
+            'dataProvider' => $dataProvider,
+        ]) ?>
         </div>
     </div>
 
