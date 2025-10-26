c286a02b066eab57979a125791e256182d8af3e8
dtavares99 <davidtavaresfs@gmail.com>
2025-10-26 16:02:35 -0300

    Feat: Add app version

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <strong>Copyright &copy; 2025 <a target="_BLANK" href="https://dtavaresfs.com">David Tavares</a>.</strong>
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0.0
+        <b>Versão:</b> <?=Yii::$app->params['appVersion']?>
     </div>
 </footer>
